9a6b205b1c3b2d57c3d15e19fb05c8191b4855d8
Muhammad Fikri <84323456+muhfik@users.noreply.github.com>
2024-07-26 22:14:08 +0700

    fix(config): edit config for dbConnection

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -2,6 +2,6 @@ require("dotenv").config();
 
 module.exports = {
 	port: process.env.PORT || 8000,
-	dbConnectionString: process.env.DB_CONNECTION_STRING,
+	dbConnectionString: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=${process.env.DB_APP_NAME}`,
 	accessTokenSecret: process.env.ACCESS_TOKEN_SECRET,
 };
