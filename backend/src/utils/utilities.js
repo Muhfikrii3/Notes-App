3c7e481fca9022e9f80aff5404aebc6b171d5688
Muhammad Fikri <84323456+muhfik@users.noreply.github.com>
2024-07-27 13:14:25 +0700

    fix(authenticateToken): authenticateToken

--- backend/src/utils/utilities.js
+++ backend/src/utils/utilities.js
@@ -1,6 +1,5 @@
 const jwt = require("jsonwebtoken");
 const config = require("../config/config");
-const User = require("../models/userModel");
 
 /**
  * Middleware to verify JWT tokens
@@ -8,7 +7,7 @@ const User = require("../models/userModel");
  * @param {Object} res - Response object
  * @param {Function} next - Next middleware function
  */
-async function authenticateToken(req, res, next) {
+function authenticateToken(req, res, next) {
 	const authHeader = req.headers["authorization"];
 	const token = authHeader && authHeader.split(" ")[1];
 
@@ -17,19 +16,14 @@ async function authenticateToken(req, res, next) {
 			.status(401)
 			.json({ error: true, message: "Token not found" });
 
-	try {
-		const decoded = jwt.verify(token, config.accessTokenSecret);
-		const user = await User.findById(decoded.userId);
-		if (!user) {
+	jwt.verify(token, config.accessTokenSecret, (err, user) => {
+		if (err)
 			return res
-				.status(404)
-				.json({ error: true, message: "User not found" });
-		}
+				.status(403)
+				.json({ error: true, message: "Invalid token" });
 		req.user = user;
 		next();
-	} catch (err) {
-		return res.status(403).json({ error: true, message: "Invalid token" });
-	}
+	});
 }
 
 module.exports = {
