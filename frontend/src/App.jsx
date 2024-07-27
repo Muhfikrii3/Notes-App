c7dafa9313eae63dfd977b8d471bb03b2204d94f
Muhammad Fikri <84323456+muhfik@users.noreply.github.com>
2024-07-28 00:11:49 +0700

    wip(Login): Login API Integration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login/Login";
 import SignUp from "./pages/SignUp/signUp";
+import Home from "./pages/Home/Home";
 
 const routes = (
 	<Router>
 		<Routes>
+			<Route path="/" exact element={<Home />} />
 			<Route path="/login" exact element={<Login />} />
 			<Route path="/signUp" exact element={<SignUp />} />
 		</Routes>
--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,10 +1,12 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Navbar from "../../components/Navbar/Navbar";
 import PasswordInput from "../../components/Input/PasswordInput";
 import { useState } from "react";
 import { validateEmail } from "../../utils/helper";
+import axiosInstance from "../../utils/axiosInstance";
 
 const Login = () => {
+	const navigate = useNavigate();
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
 	const [error, setError] = useState(null);
@@ -23,6 +25,28 @@ const Login = () => {
 		}
 
 		setError("");
+
+		try {
+			const response = await axiosInstance.post("/login", {
+				email: email,
+				password: password,
+			});
+
+			if (response.data && response.data.accessToken) {
+				localStorage.setItem("token", response.data.accessToken);
+				navigate("/");
+			}
+		} catch (error) {
+			if (
+				error.response &&
+				error.response.data &&
+				error.response.data.message
+			) {
+				setError(error.response.data.message);
+			} else {
+				setError("An unexpected error occurred. Please try again.");
+			}
+		}
 	};
 
 	return (
